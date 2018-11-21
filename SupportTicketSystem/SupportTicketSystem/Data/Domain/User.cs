6de763b000e485f81962d49a6c639407cb8689ea
Hasan <36763285+Basri96@users.noreply.github.com>
2018-11-21 12:46:10 +0100

    Add files via upload

---
+++ SupportTicketSystem/SupportTicketSystem/Data/Domain/User.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace SupportTicketSystem
+{
+    public partial class User
+    {
+        public int UserId { get; set; }
+        public string Name { get; set; }
+        public string Email { get; set; }
+        public string Password { get; set; }
+        public int Role { get; set; }
+        public string Company { get; set; }
+
+        public Role RoleNavigation { get; set; }
+    }
+}
