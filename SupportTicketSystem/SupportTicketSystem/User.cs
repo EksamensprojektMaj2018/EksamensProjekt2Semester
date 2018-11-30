56c4f4cc4bbd45dc6c162939bd1991623807bf34
Hasan Al-Basri <hasa0173@edu.easj.dk>
2018-11-30 12:33:19 +0100

    Ny EFCore run

---
+++ SupportTicketSystem/SupportTicketSystem/User.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace SupportTicketSystem
+{
+    public partial class User
+    {
+        public User()
+        {
+            Tickets = new HashSet<Ticket>();
+        }
+
+        public int UserId { get; set; }
+        public string Name { get; set; }
+        public string Email { get; set; }
+        public string Password { get; set; }
+        public int Role { get; set; }
+        public string Company { get; set; }
+
+        public Role RoleNavigation { get; set; }
+        public ICollection<Ticket> Tickets { get; set; }
+    }
+}
