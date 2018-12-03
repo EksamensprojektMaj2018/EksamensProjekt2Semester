917769f0bdc1675e3773c51d742e4d170a01c96e
Hasan Al-Basri <hasa0173@edu.easj.dk>
2018-12-03 13:40:38 +0100

    Oprettet Command klasser Oprettet userlogin klasser Lavet et login xaml

---
+++ SupportTicketSystem/SupportTicketSystem/Command/INotifiableCommand.cs
@@ -0,0 +1,10 @@
+﻿using System.Windows.Input;
+
+namespace SupportTicketSystem.Command
+{
+    // Her bruges interfacet fra Pers SimpleRPGFromScratch
+    public interface INotifiableCommand : ICommand
+    {
+        void RaiseCanExecuteChanged();
+    }
+}
---
+++ SupportTicketSystem/SupportTicketSystem/Data/Logic/UserIdLogic.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using SupportTicketSystem.Data.Base;
+
+namespace SupportTicketSystem
+{
+    public partial class User : DomainClassBase
+    {
+        public override int GetId()
+        {
+            return UserId;
+        }
+
+        public override void SetId(int id)
+        {
+            UserId = id;
+        }
+
+    }
+}
