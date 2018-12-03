352abe15ea778826e102ecdf280631322a7a77c1
Jacob Larsen <jacob.larsen6401@hotmail.dk>
2018-12-03 13:44:30 +0100

    Halvfærdig CreateTIcket fil

---
+++ SupportTicketSystem/SupportTicketSystem/ViewModel/Base/CreateTicketPageViewModelBase.cs
@@ -0,0 +1,37 @@
+﻿using System.ComponentModel;
+using System.Runtime.CompilerServices;
+using SupportTicketSystem.Annotations;
+using SupportTicketSystem.Model;
+
+namespace SupportTicketSystem.ViewModel.Base
+{
+    public class CreateTicketPageViewModelBase : INotifyPropertyChanged
+    {
+        private TicketCatalog _ticketCatalog;
+        private Ticket _newTicket;
+
+        public CreateTicketPageViewModel()
+        {
+            _ticketCatalog = new TicketCatalog();
+            _newTicket = new Ticket();
+        }
+
+        public string Message
+        {
+            get { return _newTicket.Message; }
+            set
+            {
+                _newTicket.Message = value;
+                OnPropertyChanged();
+            }
+        }
+
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        [NotifyPropertyChangedInvocator]
+        protected virtual void OnPropertyChanged([CallerMemberName] string propertyName = null)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+        }
+    }
+}
---
+++ SupportTicketSystem/SupportTicketSystem/ViewModel/Page/CreateTicketPageViewModel.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections.Generic;
+using System.ComponentModel;
+using System.Runtime.CompilerServices;
+using SupportTicketSystem.Annotations;
+using SupportTicketSystem.Model;
+
+namespace SupportTicketSystem.ViewModel.Page
+{
+    public class CreateTicketPageViewModel : INotifyPropertyChanged
+    {
+        private TicketCatalog _ticketCatalog;
+        private Ticket _newTicket;
+
+        public CreateTicketPageViewModel()
+        {
+            _ticketCatalog = new TicketCatalog();
+            _newTicket = new Ticket();
+        }
+
+        public string Message
+        {
+            get { return _newTicket.Message; }
+            set
+            {
+                _newTicket.Message = value; 
+                OnPropertyChanged();
+            }
+        }
+
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        [NotifyPropertyChangedInvocator]
+        protected virtual void OnPropertyChanged([CallerMemberName] string propertyName = null)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+        }
+    }
+}
