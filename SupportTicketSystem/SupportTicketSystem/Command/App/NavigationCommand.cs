50dec55ab31fc0f76b15e8bfa904f4a1a17f9b6b
Lasse Henriksen <lass5676@edu.easj.dk>
2018-12-05 10:15:23 +0100

    Navigation virker ordenligt nu, vry nice kanye, thank you.

---
+++ SupportTicketSystem/SupportTicketSystem/Command/App/NavigationCommand.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using Windows.UI.Xaml.Controls;
+using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
+using SupportTicketSystem.Command.Base;
+
+
+namespace SupportTicketSystem.Command.App
+{
+    public class NavigationCommand : CommandBase
+    {
+        private Frame _frame;
+        private Type _pageType;
+        private Func<bool> _canNavigate;
+
+        public NavigationCommand(Frame frame, Type pageType, Func<bool> canNavigate)
+        {
+            _frame = frame;
+            _pageType = pageType;
+            _canNavigate = canNavigate;
+        }
+        public NavigationCommand(Frame frame, Type pageType)
+            : this(frame, pageType, () => true)
+        {
+        }
+        protected override bool CanExecute()
+        {
+            return _canNavigate.Invoke();
+        }
+        protected override void Execute()
+        {
+            _frame.Navigate(_pageType);
+        }
+    }
+}
