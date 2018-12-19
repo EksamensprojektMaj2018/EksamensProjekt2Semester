e4f7695935f529783d88d49aba97bb77a65b45a2
Vitus Nørgaard Rossing Schuster <vitu0016@edu.easj.dk>
2018-12-19 10:20:51 +0100

    Unit Test Project

---
+++ SupportTicketSystem/UnitTest/UnitTest.cs
@@ -0,0 +1,37 @@
+﻿
+using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using SupportTicketSystem.Model.Catalog;
+
+namespace UnitTest
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+	        //Arrange
+	        TicketCatalog tc = new TicketCatalog();
+
+			//Act
+	        int noOfTickets = tc.All.Count;
+
+			//Assert
+	        Assert.AreEqual(4, noOfTickets);
+        }
+
+		[TestMethod]
+	    public void TestMethod2()
+	    {
+		    //Arrange
+			UserCatalog uc = new UserCatalog();
+
+			//Act
+		    int noOfUsers = uc.All.Count;
+
+		    //Assert
+			Assert.AreEqual(4, noOfUsers);
+	    }
+    }
+}
